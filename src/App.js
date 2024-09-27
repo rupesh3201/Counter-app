9650f227256e42ec280ead7dfc87e231c58c7d94
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-09-27 22:04:12 +0530

    Added image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 // componnent created
-
+// importing  img
+import Burger from  './burger.png'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <div className='Holebody' > < h1 className=' Heading'>Burger King </h1>
+    <div>
+      <img src={Burger} className='Product_image' />
+    </div>
+    </div>
+    </>
+
   )
 }
 
