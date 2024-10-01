2adc9acf6bf296e7714c65c0c11d31fd127f2fe9
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-02 02:29:51 +0530

    time to sleep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 // componnent created
 // importing  img
 import Burger from  './burger.png'
 import './App.css'
+import imgadd from './add.png';
+import imgminius from './minus.png';
+import { useState } from 'react';
 
 function App() {
+  const [Quantity , SetQuantity ] = useState(1);
+
+
+
   return (
     <>
     <div className='Holebody' > < h1 className=' Heading'>Burger King </h1>
@@ -13,7 +20,14 @@ function App() {
       <h1 className='Product_title'> Burgur</h1>
       <p className='Product_disc'>A burger, or hamburger, is a sandwich made of a patty of ground meat placed between two buns. </p>
       <p className='Product_Price'>Price: 99 ₹</p>
+      <div className='action_Btns'>
+     
+      <img src={imgminius} className='Action_btn' />
+      <p className='Product_Quantity'>1</p>
+      <img src={imgadd} className='Action_btn' />
+      </div>
       <button className='Buybtn'>Order Now</button>
+    
     </div>
     </div>
     </>
